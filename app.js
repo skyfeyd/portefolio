cf77d0e8e33bd2e94fe1094f4f8b35a907b2a094
Raphaël <raphael.castelnau@edu.devinci.fr>
2021-12-25 15:32:32 +0100

    :hammer: add wallpaper change script

---
+++ app.js
@@ -0,0 +1,20 @@
+function createHex() {
+    var hexCode1 = "";
+    var hexValues1 = "0123456789abcdef";
+    
+    for ( var i = 0; i < 6; i++ ) {
+      hexCode1 += hexValues1.charAt(Math.floor(Math.random() * hexValues1.length));
+    }
+    return hexCode1;
+  }
+  
+  function generate() {
+    
+    var deg = '90';
+    
+    var gradient = "linear-gradient(" + deg + "deg, " + "#" + createHex() + ", " + "#" + createHex() +")";
+    
+    document.getElementById('bg').style.background = gradient;
+   
+  }
+  
