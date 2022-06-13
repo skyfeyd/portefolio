ad62f66e6574abf0e5db01a2a822baaf55f61cf4
Raphael CASTELNAU <raphael.c@SIA-Conseil.local>
2022-06-13 21:34:38 +0200

    :rocket: add fetch api and contact form

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const img = document.querySelector('.img')
 const pres = document.querySelector('.pres')
 const skills = document.querySelector('.skills')
 const projects = document.querySelector('.projects')
+const contact = document.querySelector('.contact')
 
-const file = document.querySelectorAll('.file')
+const file1 = document.querySelector('.file1')
+const file2 = document.querySelector('.file2')
+const file3 = document.querySelector('.file3')
+const file4 = document.querySelector('.file4')
 const buttons = document.querySelectorAll('.buttons')
 const projectsPage = document.querySelectorAll('.project')
 
 const circle = document.querySelector('.circle')
 
+const contactButton = document.querySelector('.contactButton')
+
 const galerie = document.querySelector('.galerie')
+
+const open = document.querySelectorAll('.open')
+
+const form = document.getElementById('form')
 
 var moving = false
 var deg = '90';
@@ -24,10 +34,12 @@ var draggableP = false
 
 
 var clic = 0
+var cliqued
 
 //responsive design
 var w1 = window.matchMedia("(max-width: 550px)")
 var w2 = window.matchMedia("(min-width: 551px) and (max-width: 850px)")
+
 
 
 
@@ -51,169 +63,36 @@ document.getElementById('bg').style.animation = "gradient 15s ease infinite";
 document.getElementById('bg').style.backgroundSize = "400% 400%";
 }
 
-
-
-file.forEach(file => {
-    addEventListener('click', event => {
-        console.log(event.target.className)
-        if (event.target.className === 'file1')
-        {
-            console.log(img.classList)
-            img.classList.remove('imgc')
-            img.classList.remove('imgm')
-            img.classList.add('imgo')
-            /*
-            if(w1.matches) {
-                img.style.width = "90%"
-                img.style.height = "40%"
-                img.style.opacity = '1'
-
-                let allChildren = img.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-
-            }else if(w2.matches){
-                console.log('w2')
-                img.style.height = "40%"
-                img.style.width = "40%"
-                img.style.opacity = "1"
-
-                let allChildren = img.children
-                    for(let i=0; i < allChildren.length; i++) {
-                        allChildren[i].style.display = 'flex'
-                        //allChildren[i].style.width = '0px'
-                    }
-                
-
-                } else {
-                    img.classList.add('imgo')
-                    img.classList.remove('imgc')/*
-                    img.style.height = "40%"
-                    img.style.width = "20%"
-                    img.style.opacity = "1"
-                    //xc.style.display = "flex"
-                    //xb.style.display = "flex"
-
-                    let allChildren = img.children
-                    for(let i=0; i < allChildren.length; i++) {
-                        allChildren[i].style.display = 'flex'
-                        //allChildren[i].style.width = '0px'
-                    }
-                }
-            */
-        }  
-        if (event.target.className === 'file2') 
-        {
-            pres.classList.remove('presc')
-            pres.classList.add('preso')
-
-            /*
-            if(w1.matches) {
-                pres.style.width = '100%'
-                pres.style.height = '40%'
-                pres.style.opacity = '1'
-                //yc.style.display = 'flex'
-                //yb.style.display = 'flex'
-
-                let allChildren = pres.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-            }else{
-                pres.style.height= '34%'        
-                pres.style.width = '40%'
-                pres.style.opacity = '1'
-                //pres.style.transform = "translateX(800px)";
-                //yc.style.display = 'flex'
-                //yb.style.display = 'flex'
-
-                let allChildren = pres.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-            }*/
-        } else if (event.target.className === 'file3') {
-            skills.classList.remove('skillc')
-            skills.classList.add('skillo')/*
-            if(w1.matches) {
-                skills.style.width = '99%'
-                skills.style.height = '30%'
-                skills.style.opacity = '1'
-                skills.children[0].style.marginLeft = "0px" 
-                
-                //yc.style.display = 'flex'
-                //yb.style.display = 'flex'
-
-                let allChildren = skills.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-
-            }else{
-                skills.style.height= '26%'        
-                skills.style.width = '70%'
-                skills.style.opacity = '1'
-                //skills.style.transform = "translateY(300px)";
-                //yc.style.display = 'flex'
-                //yb.style.display = 'flex'
-
-                let allChildren = skills.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-            }*/
-        }
-        else if (event.target.className === 'file4')
-        {   
-            projects.classList.remove('projectsc')
-            projects.classList.add('projectso')/*
-            if(w1.matches) {
-                projects.style.height= 'auto'        
-                projects.style.width = '90%'
-                projects.style.opacity = '1'/*
-                galerie.style.flexDirection = "column"
-                galerie.style.position = "relative"
-                let allChildrenGal = galerie.children
-                for(let i=0; i < allChildrenGal.length; i++) {  
-                    allChildrenGal[i].style.width = '90%'
-                    allChildrenGal[i].style.margin = '5%'
-                }
-                
-                let allChildren = projects.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    //allChildren[i].style.width = '0px'
-                }
-            }else{
-                projects.style.height= '800px'        
-                projects.style.width = '90%'
-                projects.style.opacity = '1'/*
-                galerie.style.flexDirection = "row"
-                galerie.style.position = "absolute"
-                let allChildrenGal = galerie.children
-                for(let i=0; i < allChildrenGal.length; i++) {  
-                    allChildrenGal[i].style.width = '20%'
-                    allChildrenGal[i].style.margin = '5%'
-                }
-                
-                let allChildren = projects.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'flex'
-                    
-                    //allChildren[i].style.width = '0px'
-                }
-                    
-            }*/
-        }
-    })
+file1.addEventListener('click', event => {
+    cliqued = 1
+    console.log(cliqued)
+    event.preventDefault()  
+    img.classList.remove('imgc')
+    img.classList.remove('imgm')
+    img.classList.add('imgo')
 })
 
+file2.addEventListener('click', event => {
+    cliqued = 2
+    console.log(cliqued)
+    event.preventDefault()  
+    pres.classList.remove('presc')
+    pres.classList.add('preso')
+})
+
+file3.addEventListener('click', event => {
+    cliqued = 3
+    skills.classList.remove('skillc')
+    skills.classList.add('skillo')
+})
+
+file4.addEventListener('click', event => {
+    cliqued = 4
+    projects.classList.remove('projectsc')
+    projects.classList.add('projectso')
+    fetchProjects()
+})
+        
 
 
 buttons.forEach(buttons => {
@@ -252,33 +131,17 @@ buttons.forEach(buttons => {
             skills.classList.add('skillo')
         }
         if (event.target.className === "close f4"){
+            closeItems(projectsPage)
             projects.classList.remove('projectso')
-            projects.classList.add('projectsc')/*
-            projectsPage.classList.remove("open")
-            projectsPage.classList.add("close")*/
+            projects.classList.add('projectsc')
+            
         }
         if (event.target.className === "minimise f4"){
-            console.log(skills.children)
-            projects.style.height = '50px'
-            projects.style.width = '220px'
-            projects.style.padding = '0px'
-            projects.children[2].style.display = 'none'
-            /*
-            let allChildren = img.children
-                for(let i=0; i < allChildren.length; i++) {
-                    allChildren[i].style.display = 'none'
-                    //allChildren[i].style.width = '0px'
-            }*/
+            projects.classList.add('projectsm')
         }
         if (event.target.className === "bigger f4"){
-            projects.style.height= '800px'        
-            projects.style.width = '90%'
-            projects.style.opacity = '1'
-            projects.children[2].style.display = "flex"
+            projects.classList.remove('projectsm')
         }
-
-    
-        
     })
 })
 
@@ -287,26 +150,42 @@ projectsPage.forEach(projectsPage => {
     addEventListener('click', event => {
         console.log(event.target.className)
         if (event.target.className === 'project' || event.target.className === 'logo'){
-            projectsPage.classList.remove("close")
-            projectsPage.classList.add("open")
-            
-            
+            projectsPage.classList.remove('close')
+            projectsPage.classList.add('open')
         }   
-    })} 
+            }
+        )
+    } 
 )
+
+
+function closeItems(domElementsArray) {
+    domElementsArray.forEach(element => {
+        element.classList.remove('open')
+        element.classList.add('close')
+    })
+}
+
+function openItems(domElementsArray) {
+    domElementsArray.forEach(element => {
+        element.classList.add('open')
+        element.classList.remove('close')
+    })
+}
+
     
 
 
-
+/*
 dragElement(document.querySelector(".ChangeWallpaper"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
+    /* if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    /* otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
   function dragMouseDown(e) {
@@ -334,7 +213,7 @@ function dragElement(elmnt) {
   }
 
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
+    /* stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
@@ -345,5 +224,123 @@ document.onselectstart = function()
     window.getSelection().removeAllRanges();
 };
 
+*/
+
+function fetchProjects(){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        Headers: {
+            'Accept': 'application/json'
+        }
+      };
+      
+      if(cliqued === 4){
+        console.log(cliqued)
+        fetch("https://127.0.0.1:8000/api/projects", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const projectCreated = Object.getOwnPropertyNames(data)
+            console.log(projectCreated)
+            var n = -1
+            projectCreated.forEach(project => {
+                n++
+                const projectList = document.querySelector('.projectList')
+                const projectDiv = document.createElement('div')
+                projectList.append(projectDiv)
+                projectDiv.classList.add('project')
+                projectDiv.classList.add('open')
+                const MainLogoDiv = document.createElement('div')
+                projectDiv.append(MainLogoDiv)
+                MainLogoDiv.classList.add('MainLogo')
+                const logo = document.createElement('img')
+                logo.classList.add('logo')
+                logo.src = `https://127.0.0.1:8000/uploads/images/${data['hydra:member'][n]['Presentation_images']}`
+                MainLogoDiv.append(logo)
+                const contentDiv = document.createElement('div')
+                projectDiv.append(contentDiv)
+                contentDiv.classList.add('Content')
+                //contentDiv.classList.add('responsiveProject')
+                const title = document.createElement('h4')
+                title.innerHTML = data['hydra:member'][n]['Title']
+                const span = document.createElement('span')
+                const frameworkIMG = document.createElement('img')
+                frameworkIMG.src = `https://127.0.0.1:8000/uploads/images/${data['hydra:member'][n]['frameworkImages']}`
+                contentDiv.append(title)
+                contentDiv.append(span)
+                span.append(frameworkIMG)
+                const description = document.createElement('p')
+                description.innerHTML = data['hydra:member'][n]['description']
+                contentDiv.append(description)
+                const link = document.createElement('a')
+                link.href = data['hydra:member'][n]['git']
+                const git = document.createElement('img')
+                git.src = 'img/github-brands.svg'
+                link.append(git)
+                contentDiv.append(link)
+                const galerie = document.createElement('div')
+                galerie.classList.add('galerie')
+                contentDiv.append(galerie)
+                const imagesout = document.createElement('div')
+                const imagesin = document.createElement('img')  
+                imagesin.src = `https://127.0.0.1:8000/uploads/images/${data['hydra:member'][n]['Images']}`
+                imagesout.append(imagesin)
+                galerie.append(imagesout)
+            })
+        })
+        .catch(error => console.log('error', error));
+      }
+      
+}
+
+const submit = document.getElementById('submit')
 
 
+submit.addEventListener('click', (event) => {
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('message').value
+    
+    const requestOptions = {
+        method: 'POST',
+        Headers: {
+        'accept': 'application/ld+json',
+        'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+            'message': message
+        })
+    };
+    fetch("https://127.0.0.1:8000/api/contacts.json", requestOptions)
+    .then(async (response) => {
+        let json = await response.json();
+        if (response.status == 200) {
+          console.log(json.message)
+        } else {
+          console.log(response);
+        console.log(json.message)
+          
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+    }
+    
+    )
+    /*
+    axios.post('https://127.0.0.1:8000/api/contacts', {
+        name: name,
+        email: email,
+        message: message
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });*/
+})
